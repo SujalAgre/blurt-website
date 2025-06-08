8e51f7a8629f1179dcff2b5fff5ed2d18b003bb8
SujalAgre <sujalagre26@gmail.com>
2025-06-08 16:06:08 +0530

    add mobile response

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,34 @@ import githubMark from './assets/github-mark.png'
 function App() {
   return (
     <>
-      <div className='flex'>
-        <div className="w-[50vw] h-[100vh] justify-center-safe flex flex-col ml-[9vw]">
-          <img className="w-[27vw] mb-[1.4vw]" src={tryBlurt} draggable="false" />
+      <div className='flex md:flex-row sm:flex-col '>
+        <div className="sm:w-[100vw] sm:h-[100vh] md:w-[50vw] md:h-[100vh] items-center justify-center flex flex-col ">
+          <img className="sm:w-[250px] md:w-[27vw] sm:mb-[10px] md:mb-[1.4vw]" src={tryBlurt} draggable="false" />
 
-          <p className="font-bold mb-[2vw] text-[1.1vw] text-[#595959] w-[24vw]">Add custom sound effects, play them instantly, and enjoy fun times with friends!</p>
+          <p className="font-bold sm:mb-[10px] md:mb-[2vw] sm:w-[80vw] sm:text-[13px] sm:text-center md:text-[1.1vw] text-[#595959] md:w-[24vw]">Add custom sound effects, play them instantly, and enjoy fun times with friends!</p>
 
-          <p className="font-medium mb-[2.5vw] text-[1.1vw] text-[#3C3C3C] w-[25vw]">Available Platform: Android</p>
+          <p className="font-medium sm:mb-[10px] md:mb-[2.5vw] sm:text-[15px] sm:text-center md:text-[1.1vw] text-[#3C3C3C] md:w-[25vw]">Available Platform: Android</p>
 
           <div className="flex flex-row">
             <a href="https://github.com/SujalAgre/Blurt/releases/download/v1.0/blurt-app.apk" target='_blank'>
-              <button className="bg-white h-[9.5vh] w-[13vw] font-medium text-[#595959] rounded-[1.3vw] [box-shadow:0_0_0_0.3vw_#424242,0.3vw_0.25vw_0_0.2vw_#3B3B3B] text-[1.3vw] mr-[1.5vw] cursor-pointer">
+              <button className="bg-white sm:h-[7vh] sm:w-[130px] md:h-[9.5vh] md:w-[13vw] font-medium text-[#595959] sm:rounded-[13px] md:rounded-[1.3vw] sm:[box-shadow:0_0_0_3px_#424242,2.5px_2.5px_0_2px_#3B3B3B] md:[box-shadow:0_0_0_0.3vw_#424242,0.3vw_0.25vw_0_0.2vw_#3B3B3B] md:text-[1.3vw] sm:mr-[15px] md:mr-[1.5vw] cursor-pointer">
                 Download App
               </button>
             </a>
 
             <a href="https://github.com/SujalAgre/Blurt" target='_blank'>
-              <button className="bg-white h-[9.5vh] w-[5vw] font-medium text-[#595959] rounded-[1.3vw] [box-shadow:0_0_0_0.3vw_#424242,0.3vw_0.25vw_0_0.2vw_#3B3B3B] flex items-center justify-center cursor-pointer">
+              <button className="bg-white sm:h-[7vh] sm:w-[50px] sm:rounded-[13px] md:h-[9.5vh] md:w-[5vw] font-medium text-[#595959] md:rounded-[1.3vw] sm:[box-shadow:0_0_0_3px_#424242,2.5px_2.5px_0_2px_#3B3B3B] md:[box-shadow:0_0_0_0.3vw_#424242,0.3vw_0.25vw_0_0.2vw_#3B3B3B] flex items-center justify-center cursor-pointer">
                 <img src={githubMark} alt="GitHub Logo" className="h-[60%] w-auto" draggable="false" />
               </button>
             </a>
-
-
           </div>
+
         </div>
-        <div className="w-[50vw] h-[100vh] items-center justify-center flex absolute right-[1.5vw]">
-          <img className="w-[23vw]" src={Screens} draggable="false" />
+
+        <div className="md:w-[50vw] sm:hidden md:h-[100vh] items-center justify-center md:flex md:mr-15">
+          <img className="sm:w-[200px] md:w-[23vw]" src={Screens} draggable="false" />
         </div>
+
       </div>
     </>
   )
